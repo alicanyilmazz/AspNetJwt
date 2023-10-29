012c97b7e8edd5b32f726f6175dee8e796bdca42
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-30 00:06:50 +0300

    BaseController created.

---
+++ Infrastructure/Controller/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Controller.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Infrastructure.Controller
+{
+    public class BaseController : ApiController, IAuthorizationValidator
+    {
+        public bool Authenticate()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            if (identity == null)
+            {
+                return false;
+            }
+            if (!identity.IsAuthenticated)
+            {
+                return false;
+            }
+
+            return CheckClaims(identity);
+        }
+
+        public virtual bool CheckClaims(ClaimsIdentity claims)
+        {
+            return true;
+        }
+    }
+}
