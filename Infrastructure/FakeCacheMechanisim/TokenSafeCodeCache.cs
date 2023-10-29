6ba9d2c925bdf28f9cef4f805f4778964da21e64
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-29 20:35:56 +0300

    TokenSafeCodeCache created.

---
+++ Infrastructure/FakeCacheMechanisim/TokenSafeCodeCache.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Common;
+using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.FakeCacheMechanisim
+{
+    public class TokenSafeCodeCache
+    {
+        public static (bool, TokenSafeCode) GetSafeDefinition(string grantType)
+        {
+            var safeCodes = new Dictionary<string, TokenSafeCode>()
+            {
+                {"terminal_credentials",new TokenSafeCode(){ UsernameCode = "TERMINAL_USERNAME", PasswordCode = "TERMINAL_PASSWORD" } },
+                {"server_credentials",new TokenSafeCode(){ UsernameCode = "SERVER_USERNAME", PasswordCode = "SERVER_PASSWORD" } },
+            };
+
+            if (!string.IsNullOrEmpty(grantType) && safeCodes.ContainsKey(grantType))
+            {
+                return (true, safeCodes[grantType]);
+            }
+
+            return (false, new TokenSafeCode());
+        }
+    }
+}
