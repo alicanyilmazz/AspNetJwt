58d51d2ac5218bd4bfc2efcf55e77060ca7b4d54
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-29 20:36:28 +0300

    RefreshTokenResponseMessage created.

---
+++ Infrastructure/Models/RefreshTokenResponseMessage.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class TokenResponseMessage
+    {
+        public string TokenType { get; set; }
+        public string AccessToken { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class RefreshTokenResponseMessage : TokenResponseMessage
+    {
+        public string RefreshToken { get; set; }
+        public int RefreshTokenExpiresIn { get; set; }
+    }
+}
