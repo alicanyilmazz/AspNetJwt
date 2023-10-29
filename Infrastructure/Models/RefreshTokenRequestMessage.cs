1f3a1c6fb9aad7cb1aff7eba905320c51f933464
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-30 00:08:20 +0300

    RefreshTokenRequestMessage created

---
+++ Infrastructure/Models/RefreshTokenRequestMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class RefreshTokenRequestMessage
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
