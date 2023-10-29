62298bd189d83c1516b434ff6cacd25d8a3db906
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-30 00:07:52 +0300

    ErrorCode const created.

---
+++ Infrastructure/GlobalVariables/Constants/AuthorizationErrorCodes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.GlobalVariables.Constants
+{
+    public class AuthorizationErrorCodes
+    {
+        public static readonly int UNAUTHORIZED_ERROR_CODE = -1;
+    }
+}
