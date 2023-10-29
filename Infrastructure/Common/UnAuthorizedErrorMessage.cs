676c99e07e8cc3d7ae00ff0b19564469ad3075ed
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-29 20:27:40 +0300

    UnAuthorizedErrorMessage created.

---
+++ Infrastructure/Common/UnAuthorizedErrorMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class UnAuthorizedErrorMessage
+    {
+        public string Message { get; set; }
+        public int Code { get; set; }
+    }
+}
