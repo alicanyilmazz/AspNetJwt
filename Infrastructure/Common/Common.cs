bdc2c410d2606c68f0c4d2df505dd66510ebec93
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-29 20:27:12 +0300

    ExceptionHelper created.

---
+++ Infrastructure/Common/Common.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Infrastructure.Common
+{
+    public class ExceptionHelper
+    {
+        public static HttpResponseException GenerateUnAuthorizeException(string message , int code)
+        {
+            HttpResponseMessage unauthorizedHttpResponse = GenerateUnAuthorizedHttpMessage(message, code);
+            return new HttpResponseException(unauthorizedHttpResponse);
+        }
+        public static HttpResponseMessage GenerateUnAuthorizedHttpMessage(string message , int code)
+        {
+            const string responseCode = "authorizationerror";
+            var responseContent = JsonConvert.SerializeObject(new UnAuthorizedErrorMessage { Message = message, Code = code });
+            var unauthorizedHttpResponse = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+            {
+                ReasonPhrase = responseCode,
+                Content = new StringContent(responseContent, Encoding.UTF8, "application/json")
+            };
+            //Logger.WarnFormat("authorizationerror {0}",message);
+            return unauthorizedHttpResponse;
+        }
+
+    }
+}
