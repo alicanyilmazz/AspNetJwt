dc4abff775b2ce254ee32895cb0f6ba1aa0b2384
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-29 20:36:39 +0300

    TokenSafeCode created.

---
+++ Infrastructure/Models/TokenSafeCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class TokenSafeCode
+    {
+        public string UsernameCode { get; set; }
+        public string PasswordCode { get; set; }
+    }
+}
