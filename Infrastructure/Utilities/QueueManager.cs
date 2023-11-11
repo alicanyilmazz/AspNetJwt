1d189e1a4f8d72b38b5da1e386752ac3fba387f9
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-12 01:43:28 +0300

    QueueManager created.

---
+++ Infrastructure/Utilities/QueueManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Utilities
+{
+    public interface IQueueManager
+    {
+        void Publish(string queueName, byte[] message);
+        void Publish(Uri queueUri, string queueName, byte[] message);
+    }
+    public class QueueManager: IQueueManager
+    {
+        public void Publish(string queueName, byte[] message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Publish(Uri queueUri, string queueName, byte[] message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
