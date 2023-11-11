a602ae4c4d3cbcd502e279a3e564b2e443088df8
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-11 21:58:40 +0300

    ITokenProvider created.

---
+++ Infrastructure/Rest/ITokenProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Rest
+{
+    public interface ITokenProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="TimeoutException"></exception>
+        string GetToken();
+        void ResetToken();
+        void AddClientCertificate(string certificateThumbprint , StoreName storeName, StoreLocation location);
+    }
+}
