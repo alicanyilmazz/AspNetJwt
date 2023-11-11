7c1852a8fabf4b74658d2d1c5f6bc8e5bf652da5
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-12 01:39:21 +0300

    CoreTokenRequestMessage created.

---
+++ AuthorizationApp/Core/CoreTokenRequestMessage.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthorizationApp.Core
+{
+    public class CoreTokenRequestMessage : BasicTokenRequestMessage
+    {
+        public string IpAddress { get; set; }
+    }
+}
