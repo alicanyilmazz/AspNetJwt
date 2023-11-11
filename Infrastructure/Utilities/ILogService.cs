45207e9ccdc66a9059f13a0d7bdb70104559bc2c
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-12 01:43:10 +0300

    ILogService creetad.

---
+++ Infrastructure/Utilities/ILogService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Utilities
+{
+    public interface ILogService : IDisposable
+    {
+        bool IsDebugEnabled();
+        void Debug(object message);
+        void Debug(string message, Exception exception);
+        void DebugFormat(string format, params object[] args);
+        void DebugFormat(IFormatProvider provider, string format, params object[] args);
+        void Error(object message);
+        void Error(string message, Exception exception);
+        void ErrorFormat(string format, params object[] args);
+        void ErrorFormat(IFormatProvider provider, string format, params object[] args);
+        void Fatal(object message);
+        void Fatal(string message, Exception exception);
+        void FatalFormat(string format, params object[] args);
+        void FatalFormat(IFormatProvider provider, string format, params object[] args);
+        void Info(object message);
+        void Info(string message, Exception exception);
+        void InfoFormat(string format, params object[] args);
+        void InfoFormat(IFormatProvider provider, string format, params object[] args);
+        void Warn(object message);
+        void Warn(string message, Exception exception);
+        void WarnFormat(string format, params object[] args);
+        void WarnFormat(IFormatProvider provider, string format, params object[] args);
+
+    }
+}
