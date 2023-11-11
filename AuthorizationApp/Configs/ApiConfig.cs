785d838d08c312910d104a6d2f8c02e70b1fe36a
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-12 01:38:55 +0300

    ApiConfig created.

---
+++ AuthorizationApp/Configs/ApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Filters;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AuthorizationApp.Configs
+{
+    public class ApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.SuppressDefaultHostAuthentication();
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new DefaultContractResolver { IgnoreSerializableAttribute = true };
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            config.MapHttpAttributeRoutes();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.Filters.Add(new HostAuthorizeAttribute());
+            HostAuthorizeAttribute.HostAuthorizationSettingKey = "UseHostCoreAuthorization";
+        }
+    }
+}
