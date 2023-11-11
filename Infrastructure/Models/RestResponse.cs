b7d38135fa9506bee373f77b822f3915478fa19a
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-11 21:57:17 +0300

    RestResponse created.

---
+++ Infrastructure/Models/RestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class RestResponse<T>
+    {
+        public T Body { get; set; }
+        public WebHeaderCollection Header { get; set; }
+    }
+    public class RestResponse
+    {
+        public string Body { get; set; }
+        public WebHeaderCollection Header { get; set; }
+    }
+}
